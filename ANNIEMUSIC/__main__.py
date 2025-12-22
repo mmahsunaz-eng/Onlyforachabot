d65f5b9ae51bdf53d9f8830685a74d582f90acfb
mmahsunaz-eng <mmahsun.az@gmail.com>
2025-12-22 11:23:52 +0700

    Update __main__.py

--- ANNIEMUSIC/__main__.py
+++ ANNIEMUSIC/__main__.py
@@ -13,7 +13,6 @@ from ANNIEMUSIC.misc import sudo
 from ANNIEMUSIC.plugins import ALL_MODULES
 from ANNIEMUSIC.utils.database import get_banned_users, get_gbanned
 from ANNIEMUSIC.utils.cookie_handler import fetch_and_store_cookies
-from config import store_cookie
 
 JARVIS = StreamController()
 
